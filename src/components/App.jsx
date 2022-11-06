d4c1bcb9d9f5edf0ab8b34e5e54f085f756aa471
VKlapan <vladimir.klapan@gmail.com>
2022-11-06 10:13:50 +0200

    add lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { lazy, Suspense } from 'react';
 
-import SharedLayout from 'pages/SharedLayout/SharedLayout';
-import ContactsListPage from 'pages/ContactsListPage/ContactsListPage';
-import HomePage from 'pages/HomePage/HomePage';
-import AboutPage from 'pages/AboutPage/AboutPage';
-import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import LoginPage from 'pages/LoginPage/LoginPage';
 import { checkCurrent } from 'redux/authorization/authorizationOperations';
 import { getIsCheckingCurrent } from 'redux/authorization/authorizationsSelectors';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
+
+const SharedLayout = lazy(() => import('pages/SharedLayout/SharedLayout'));
+const ContactsListPage = lazy(() =>
+  import('pages/ContactsListPage/ContactsListPage')
+);
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const AboutPage = lazy(() => import('pages/AboutPage/AboutPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -26,20 +30,22 @@ const App = () => {
       {isCheckingUser ? (
         <p>Loading ...</p>
       ) : (
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/contacts" element={<ContactsListPage />} />
+        <Suspense fallback={<p>...Load page</p>}>
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/contacts" element={<ContactsListPage />} />
+              </Route>
+              <Route element={<PublicRoute />}>
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+              </Route>
+              <Route path="*" element={<HomePage />} />
             </Route>
-            <Route element={<PublicRoute />}>
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-            </Route>
-            <Route path="*" element={<HomePage />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       )}
     </>
   );
